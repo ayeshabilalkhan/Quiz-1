265330f24f12500ab0cd7cf1a434376fae89f80e
ayeshabilalkhan <ayeshabilalkhan7@gmail.com>
2024-07-17 09:41:47 -0700

    first commit

---
+++ app.ts
@@ -0,0 +1,15 @@
+// // 1:
+let isCompleted: boolean = false;
+isCompleted = true;
+console.log(isCompleted); // true (ans)
+
+// 2:
+let firstName:string = "Ali";
+let lastName:string = "Sara";
+let fullName: string =firstName+" "+lastName;
+
+console.log(fullName); // Ali Sara (ans)
+
+// 3:
+let age: number = 25;
+console.log("Age:"+ age); // Age:25 (ans)
